add18be16c2816bf67633507da512ed5f63685e2
Jayant211998 <jayantgawali98@gmail.com>
2023-04-12 10:26:35 +0530

    logo

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -184,7 +184,15 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           </MDTypography>
         </MDBox>
         <MDBox component={NavLink} to="/" display="flex" alignItems="center">
-          {brand && <MDBox component="img" src={brand} alt="Brand" width="2rem" />}
+          {brand && (
+            <MDBox
+              component="img"
+              src={brand}
+              alt="Brand"
+              width="2rem"
+              style={{ marginRight: "0.5rem", borderRadius: "1rem" }}
+            />
+          )}
           <MDBox
             width={!brandName && "100%"}
             sx={(theme) => sidenavLogoLabel(theme, { miniSidenav })}
